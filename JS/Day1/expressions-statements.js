b7e7fc6370a895eaf0c7e36d98060c20afef40e1
5uba <v.subashini.2004@gmail.com>
2026-09-10 12:13:05 +0530

    Updated project

--- JS/Day1/expressions-statements.js
+++ JS/Day1/expressions-statements.js
@@ -2,52 +2,52 @@
 // Expression : An expression is a piece of code that produces a value.
 
 // Arithmetic Expressions
-10 + 20;
-50 - 10;
-5 * 4;
-20 / 5;
-10 % 3;
+// 10 + 20;
+// 50 - 10;
+// 5 * 4;
+// 20 / 5;
+// 10 % 3;
 
 // Variable Expressions
-age;
-age + 5;
-age * 2;
+// age;
+// age + 5;
+// age * 2;
 
 // Comparison Expressions
-age > 18;
-age < 18;
-age === 21;
-age !== 20;
-age >= 18;
+// age > 18;
+// age < 18;
+// age === 21;
+// age !== 20;
+// age >= 18;
 
 // Logical Expressions
-age > 18 && age < 30;
-age > 25 || age < 10;
-!(age < 18);
+// age > 18 && age < 30;
+// age > 25 || age < 10;
+// !(age < 18);
 
 // String Expressions
-"Hello";
-"JavaScript";
-"Hello " + "World";
+// "Hello";
+// "JavaScript";
+// "Hello " + "World";
 
 // Array Expression
-[10, 20, 30];
+// [10, 20, 30];
 
 // Object Expression
-{
-    name: "Suba",
-    age: 21
-}
+// {
+//     name: "Suba",
+//     age: 21
+// }
 
 // Function Expression
-console.log("Hello");
+// console.log("Hello");
 
 //Statement : A statement is a complete instruction given to JavaScript.
 // Variable Declaration Statement
-let name = "Suba";
+// let name = "Suba";
 
 // Assignment Statement
-age = 22;
+// age = 22;
 
 // Console Statement
 console.log(name);
